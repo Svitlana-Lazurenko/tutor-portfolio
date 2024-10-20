e4cfcf5f9bf52aa362c0dfa88a7bdab86ae3a20e
Svitlana Lazurenko <lazurenko.svitlana.v@gmail.com>
2024-10-20 16:01:46 +0300

    Develop "advantages"

--- template-parts/home-page-advantages.php
+++ template-parts/home-page-advantages.php
@@ -2,34 +2,30 @@
 if (!defined('ABSPATH')) exit; ?>
 
 <section id="advantages" class="advantages">
-    <div class="container container--outer">
-        <div class="advantages__img-1" aria-hidden="true">
+
+    <div class="container">
+        <div class="advantages__title-wrapper">
+            <h2 id="advantages" class="advantages__title">Переваги курсу</h2>
         </div>
-        <div class="advantages__img-2" aria-hidden="true">
+
+        <div class="advantages__img" aria-hidden="true">
         </div>
 
+        <?php $advantages = get_field('advantages'); ?>
 
-
-        <div class="container">
-            <div class="advantages__title-wrapper">
-                <h2 id="advantages" class="advantages__title">Переваги курсу</h2>
-            </div>
-
-            <?php $advantages = get_field('advantages'); ?>
-
-            <?php if (have_rows('advantages')) : ?>
-                <ol class="advantages-list">
-                    <?php while (have_rows('advantages')) : the_row();
-                        $advantage = get_sub_field('advantage');
-                    ?>
-                        <li class="advantages-list__item">
-                            <?php if ($advantage <> '') : ?>
-                                <div class="advantages-list__text"><?php echo $advantage; ?></div>
-                            <?php endif; ?>
-                        </li>
-                    <?php endwhile; ?>
-                </ol>
-            <?php endif; ?>
-        </div>
+        <?php if (have_rows('advantages')) : ?>
+            <ol class="advantages-list">
+                <?php while (have_rows('advantages')) : the_row();
+                    $advantage = get_sub_field('advantage');
+                ?>
+                    <li class="advantages-list__item">
+                        <?php if ($advantage <> '') : ?>
+                            <div class="advantages-list__text"><?php echo $advantage; ?></div>
+                        <?php endif; ?>
+                    </li>
+                <?php endwhile; ?>
+            </ol>
+        <?php endif; ?>
     </div>
+
 </section>
